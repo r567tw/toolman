2ff6f424eb806106a16800319dc40e22025ae250
r567tw <r567tw@gmail.com>
2022-07-20 11:34:30 +0800

    [add] swagger doc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="土爾曼API文件",
+ *      description="測試Swagger文件",
+ *      @OA\Contact(
+ *          email="r567tw@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ * @OA\Tag(name="Ping", description="ping")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
 }
--- app/Http/Controllers/PongController.php
+++ app/Http/Controllers/PongController.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 class PongController extends Controller
 {
-    public function ping(){
+    /**
+     * @OA\Get(
+     *      path="/api/ping",
+     *      operationId="ping",
+     *      tags={"Ping"},
+     *      summary="Health checker",
+     *      description="Health checker",
+     *      @OA\Response(
+     *          response=200,
+     *          description="請求成功"
+     *       )
+     * )
+     * Returns pong
+     */
+    public function ping()
+    {
         return response()->json(['message' => 'pong']);
     }
-
-
 }
